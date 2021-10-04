6876af7819138da7c00f550cbf18d4ee5465273d
Rick <ricksmycodeguy@gmail.com>
2021-10-04 09:59:23 -0500

    testing server

--- api/server.js
+++ api/server.js
@@ -8,8 +8,23 @@ server.use(express.json());
 
 server.use("/api/accounts", AccountsRouter)
 
-server.get("/", (req, res) =>{
-    res.status(200).json({ api: "Api up and running"})
+// server.get('/', (req, res) => {
+//     res.json({ message: 'Api is working add a route to see more' });
+//     console.log('api is working');
+// })
+
+server.use("*", (req,res)=>{
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
 })
 
 module.exports = server;
