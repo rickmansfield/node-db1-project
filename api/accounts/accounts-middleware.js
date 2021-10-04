3812ce954ee786e9c7d182e65174fb0e3817d3cd
Rick <ricksmycodeguy@gmail.com>
2021-10-04 15:55:25 -0500

    build out checkAccountPayload middlware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,36 +1,48 @@
-const Accounts = require ('./accounts-model')
+const Accounts = require('./accounts-model')
 
-function checkAccountPayload (req, res, next) {
+function checkAccountPayload(req, res, next) {
   console.log('checkAccountPayload');
+  const error = { status: 400 }
   const { name, budget } = req.body
-  if(name && budget){
-    next();
-  }else{
-    res.status(400).json({ 
-      message: "name and budget are required"
-    })
+  if (name === undefined || budget === undefined) {
+    error.message = 'name and budget are required'
+  } else if (typeof name !== 'string') {
+    error.message = 'name of account must be a string'
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    error.message = 'name of account must be between 3 and 100'
+  } else if (typeof budget !== 'number' || isNaN(budget)) {
+    error.message = "budget of account must be a number"
+  } else if (budget < 0 || budget > 1000000) {
+    error.message = "budget of account is too large or too small"
+  }
+  if (error.message) {
+    next(error)
+  } else {
+    next()
   }
 }
 
-function checkAccountNameUnique (req, res, next) {
+function checkAccountNameUnique(req, res, next) {
   console.log('checkAccountNameUnique middleware');
+  const { name } = req.body
+  if(name === name)
   next()
 }
 
-async function checkAccountId (req, res, next) {
+async function checkAccountId(req, res, next) {
   console.log('checkAccountId middleware');
-  try{
+  try {
     const account = await Accounts.getById(req.params.id)
-    if(account){
+    if (account) {
       req.account = account
       next()
     }
-    else{
-      res.status(404).json({message:"account not found"})
-    } 
-  }catch(err){
+    else {
+      res.status(404).json({ message: "account not found" })
+    }
+  } catch (err) {
     next(err)
   }
 }
 
-module.exports = {checkAccountPayload, checkAccountNameUnique, checkAccountId}
+module.exports = { checkAccountPayload, checkAccountNameUnique, checkAccountId }
